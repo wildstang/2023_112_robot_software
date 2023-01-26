ceb0c4ba055764062e2bd8d461cb1745a7e7871a
Lwannebo5476 <90660369+Lwannebo5476@users.noreply.github.com>
2023-01-26 17:48:59 -0600

    removed follower, added boolean, disabled presets

--- src/main/java/org/wildstang/year2023/subsystems/arm.java
+++ src/main/java/org/wildstang/year2023/subsystems/arm.java
@@ -5,6 +5,7 @@ import org.wildstang.framework.subsystems.Subsystem;
 import org.wildstang.hardware.roborio.inputs.WsDigitalInput;
 import org.wildstang.hardware.roborio.inputs.WsJoystickAxis;
 import org.wildstang.hardware.roborio.outputs.WsSparkMax;
+import org.wildstang.hardware.roborio.outputs.config.WsSparkMaxFollowerConfig;
 import org.wildstang.year2023.robot.WSInputs;
 import org.wildstang.year2023.robot.WSOutputs;
 
@@ -17,12 +18,15 @@ private WsDigitalInput buttonB;
 private WsDigitalInput buttonC;
 private double motorSpeed;
 private int position;
+private boolean preset;
 
 
     @Override
     public void init() {
 
-        motor = (WsSparkMax) WSOutputs.TEST_MOTOR.get();
+        motor = (WsSparkMax) WSOutputs.ARM.get();
+        motor.initClosedLoop(0.5, 0, 0, 0);
+        motor.setCurrentLimit(40, 40, 0);
         joystick = (WsJoystickAxis) WSInputs.MANIPULATOR_LEFT_JOYSTICK_Y.get();
         buttonA = (WsDigitalInput) WSInputs.MANIPULATOR_DPAD_UP.get();
         buttonB = (WsDigitalInput) WSInputs.MANIPULATOR_DPAD_LEFT.get();
@@ -33,26 +37,32 @@ private int position;
     @Override
     public void resetState() {
         position = 0;
+        motorSpeed = 0;
+        preset = false;
     }
 
     @Override
     public void inputUpdate(Input source) {
 
-        if (source == joystick) {
+        if (source == joystick && (Math.abs(joystick.getValue()) > 0.10)) {
 
             motorSpeed = joystick.getValue();
+            preset = false;
 
         }
 
         //presets
         if (source == buttonA) { 
             position = 1;
+            preset = true;
         }
         if (source == buttonB) { 
             position = 2;
+            preset = true;
         }
         if (source == buttonC) { 
             position = 3;
+            preset = true;
         }
 
 
@@ -61,17 +71,21 @@ private int position;
     @Override
     public void update() {
 
-        motor.setValue(motorSpeed);
-
-        if (position == 1){
+        if(preset == false){
+            motor.setValue(motorSpeed);
+        }
+        
+        /* 
+        if (position == 1 && preset == true){
             motor.setPosition(000);
         }
-        if (position == 2){
+        if (position == 2 && preset == true){
             motor.setPosition(000);
         }
-        if (position == 3){
+        if (position == 3 && preset == true){
             motor.setPosition(000);
         }
+        */
 
     }
 
