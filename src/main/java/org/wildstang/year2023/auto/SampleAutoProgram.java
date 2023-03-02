cfde3125d37076e5aed1853973465b4c0795f292
ARMS300 <90659881+ARMS300@users.noreply.github.com>
2023-03-01 20:34:14 -0600

    Added auto steps and imported Pathing from Pathing app

--- src/main/java/org/wildstang/year2023/auto/SampleAutoProgram.java
+++ src/main/java/org/wildstang/year2023/auto/SampleAutoProgram.java
@@ -1,8 +1,13 @@
 package org.wildstang.year2023.auto;
 
 import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.framework.auto.AutoStep;
 import org.wildstang.framework.auto.steps.PathFollowerStep;
 import org.wildstang.framework.core.Core;
+import org.wildstang.year2023.auto.Steps.ClawRelease;
+import org.wildstang.year2023.auto.Steps.IntakeCubeStep;
+import org.wildstang.year2023.auto.Steps.MoveArm;
+import org.wildstang.year2023.auto.Steps.OuttakeCubeStep;
 import org.wildstang.year2023.robot.WSSubsystems;
 import frc.paths.*;
 
@@ -12,8 +17,23 @@ import frc.paths.*;
  */
 public class SampleAutoProgram extends AutoProgram {
 
+    
+
     @Override
     protected void defineSteps() {
+        //Scores cone Preload
+        addStep(new MoveArm("MID"));
+        addStep(new ClawRelease(true));
+        addStep(new ClawRelease(false));
+        addStep(new MoveArm("STOW"));
+
+        //Pickup cube from field
+        addStep(new IntakeCubeStep(true));
+        addStep(new IntakeCubeStep(false));
+        
+        //score cube
+        addStep(new OuttakeCubeStep(true));
+        addStep(new OuttakeCubeStep(false));
     }
 
     @Override
