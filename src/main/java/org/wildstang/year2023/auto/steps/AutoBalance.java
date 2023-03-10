717c5885f16f04bc270852d62fb0b120447d1e53
Zaque2 <5573103+Zaque2@users.noreply.github.com>
2023-03-10 14:07:38 -0600

    Update auto balance to call setFinished when balanced

--- src/main/java/org/wildstang/year2023/auto/steps/AutoBalance.java
+++ src/main/java/org/wildstang/year2023/auto/steps/AutoBalance.java
@@ -5,6 +5,7 @@ import org.wildstang.framework.core.Core;
 import org.wildstang.year2023.robot.WSSubsystems;
 import org.wildstang.year2023.subsystems.swerve.SwerveDrive;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoBalance extends AutoStep {
@@ -44,9 +45,9 @@ public class AutoBalance extends AutoStep {
         SmartDashboard.putNumber("gyro balance output", error);
         SmartDashboard.putNumber("gyro d term", dOut);
         
-        if (Math.abs(error) < 3) { // || DriverStation.getMatchTime() <= 0.1
+        if (Math.abs(error) < 3|| DriverStation.getMatchTime() <= 0.1) { // 
             swerve.setCross();
-            // setFinished();
+            setFinished();
         } else {
             swerve.setAutoValues(Math.abs(output), heading);
         }
